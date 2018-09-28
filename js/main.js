1f52b70e7577ba2ed4b5cc0cfa46bd00bdaa66e6
Davida Pitts <Davida@orangemooncreative.com>
2018-09-28 12:35:18 +0800

    Add accessibility features

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.alt;
   li.append(image);
 
   const name = document.createElement('h1');
@@ -177,6 +178,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.tabIndex = '4';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
